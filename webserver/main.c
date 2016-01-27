04e5099933c953076d786e1190e9f0f7bfcb67ee
Nicolas Pamart <nicolaspmt@gmail.com>
2016-01-27 17:30:30 +0100

    Pair programming avec EtienneChapitre 3 ok il faut encore faire les questions

--- webserver/main.c
+++ webserver/main.c
@@ -1,12 +1,41 @@
 #include <stdio.h>
 #include <string.h>
+#include "socket.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <strings.h>
 
-int main(int argc, char **argv) {
-	/* Arnold Robbins in the LJ of February ’95 , describing RCS */
-	if(argc > 1 && strcmp (argv[1], "-advice") == 0) {
-		printf("Don't Panic !\n");
-		return 42;
+#define BUF_SIZE 1024
+
+int main(void){
+	int socket_serveur = creer_serveur(8080);
+	if(socket_serveur == -1)
+		return -1;
+	int socket_client;
+	socket_client = accept(socket_serveur, NULL, NULL);
+	if (socket_client == -1){
+		perror("accept ");
+		return 1;
 	}
-	printf("Need an advice ?\n");
+	/* On peut maintenant dialoguer avec le client */
+	const char *message_bienvenue = "Bonjour, bienvenue sur le serveur Cowboy. Ce serveur est créé pour remplacer Apache.\nLes cowboys ont tués les Indiens d'Amérique et se sont appropriés leurs terres.\n Le cow-boy ou cowboy, de l'anglais cow, « vache » et boy, « garçon »), qui signifie vacher ou bouvier en français, est un garçon de ferme s'occupant du bétail bovin dans les pays anglo-saxons de grands espaces comme le Far West américain et l'Outback australien.\n";
+	write(socket_client, message_bienvenue, strlen(message_bienvenue));
+	
+	char buf[BUF_SIZE];
+	while(1){
+		bzero(buf, BUF_SIZE);
+		int n = read(socket_client, buf, BUF_SIZE-1);
+		if(n == -1){
+			perror("read socket");
+			return 1;	
+		}
+		buf[BUF_SIZE-1] = '\0';
+		printf("%s", buf);
+		write(socket_client, buf, n);
+	}
+
+	close(socket_client);
+	close(socket_serveur);
 	return 0;
 }
--- webserver/socket.c
+++ webserver/socket.c
@@ -1,1 +1,29 @@
-// Autor : Etienne
+// Autor : Etienne (Nicolas)
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
+#include "socket.h"
+#include <stdio.h>
+#include <arpa/inet.h>
+
+int creer_serveur(int port){
+	int socket_serveur ;
+	socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
+	if (socket_serveur == -1){
+		perror("socket_serveur");
+		return -1;
+	}
+	/* Utilisation de la socket serveur */
+	struct sockaddr_in saddr ;
+	saddr.sin_family = AF_INET ; /* Socket ipv4 */
+	saddr.sin_port = htons (port); /* Port d ’ écoute */
+	saddr.sin_addr.s_addr = INADDR_ANY; /* écoute sur toutes les interfaces */
+	if (bind(socket_serveur, (struct sockaddr *)&saddr, sizeof(saddr)) == -1){
+		perror("bind socket_serveur");
+		return -1;	
+	}
+	if (listen(socket_serveur, 10) == -1){
+		perror("listen socket_serveur");
+		return -1;
+	}	
+	return socket_serveur;
+}
