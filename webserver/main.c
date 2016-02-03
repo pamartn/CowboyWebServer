f3707eacd9b7ed7b397ca49aaf62e48f27665e68
Nicolas Pamart <nicolaspmt@gmail.com>
2016-02-03 15:56:39 +0100

    Super serveur avec des forks

--- webserver/main.c
+++ webserver/main.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <strings.h>
 #include <signal.h>
+#include <stdlib.h>
 
 #define BUF_SIZE 1024
 
@@ -15,41 +16,44 @@ void initialiser_signaux(void) {
 	}
 }
 int main(void){
+	const char *message_bienvenue = "Bonjour, bienvenue sur le serveur Cowboy. Ce serveur est créé pour remplacer Apache.\nLes cowboys ont tués les Indiens d'Amérique et se sont appropriés leurs terres.\n Le cow-boy ou cowboy, de l'anglais cow, « vache » et boy, « garçon »), qui signifie vacher ou bouvier en français, est un garçon de ferme s'occupant du bétail bovin dans les pays anglo-saxons de grands espaces comme le Far West américain et l'Outback australien.\n";
+	
 	initialiser_signaux();
 	int socket_serveur = creer_serveur(8080);
 	if(socket_serveur == -1)
 		return 1;
+	
 	int socket_client;
-	socket_client = accept(socket_serveur, NULL, NULL);
-	if (socket_client == -1){
-		perror("accept ");
-		return 1;
-	}
-	/* On peut maintenant dialoguer avec le client */
-	const char *message_bienvenue = "Bonjour, bienvenue sur le serveur Cowboy. Ce serveur est créé pour remplacer Apache.\nLes cowboys ont tués les Indiens d'Amérique et se sont appropriés leurs terres.\n Le cow-boy ou cowboy, de l'anglais cow, « vache » et boy, « garçon »), qui signifie vacher ou bouvier en français, est un garçon de ferme s'occupant du bétail bovin dans les pays anglo-saxons de grands espaces comme le Far West américain et l'Outback australien.\n";
-	sleep(1);
-	int testErrorWrite = write(socket_client, message_bienvenue, strlen(message_bienvenue));
-	if (testErrorWrite == -1) {
-		perror("error write :");
-		return 1;
-	}
-	char buf[BUF_SIZE];
 	while(1){
-		bzero(buf, BUF_SIZE);
-		int n = read(socket_client, buf, BUF_SIZE-1);
-		if(n == -1){
-			perror("read socket");
-			return 1;	
+		socket_client = accept(socket_serveur, NULL, NULL);
+		if (socket_client == -1){
+			perror("accept ");
+			return 1;
 		}
-		buf[BUF_SIZE-1] = '\0';
-		printf("%s", buf);
-		write(socket_client, buf, n);
+		if(fork() == 0){
+			/* On peut maintenant dialoguer avec le client */
+			int testErrorWrite = write(socket_client, message_bienvenue, strlen(message_bienvenue));
+			if (testErrorWrite == -1) {
+				perror("error write :");
+				exit(1);
+			}
+			char buf[BUF_SIZE];
+			int n;
+			bzero(buf, BUF_SIZE);
+			while((n = read(socket_client, buf, BUF_SIZE-1)) > 0){
+				buf[BUF_SIZE-1] = '\0';
+				printf("%s", buf);
+				if(write(socket_client, buf, n) <= 0)
+					break;
+				bzero(buf, BUF_SIZE);
+			}
+			perror("socket closed");
+			close(socket_client);
+			exit(1);
+		} else {
+			close(socket_client);
+		}
 	}
-
-	close(socket_client);
 	close(socket_serveur);
 	return 0;
 }
-
-
-
