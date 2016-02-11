a20def9d9687023eef9fbb78b4c31eceaf8d786b
Nicolas Pamart <nicolaspmt@gmail.com>
2016-02-11 11:36:28 +0100

    Added debug var to properly print debug log

---
+++ webserver/debug.h
@@ -0,0 +1,13 @@
+#ifndef __DEBUG_H__
+# define __DEBUG_H__
+
+
+//#define DEBUG
+
+#ifdef DEBUG
+#define DEBUG_PRINT(fmt, args...)    fprintf(stderr, fmt, ## args)
+#else
+#define DEBUG_PRINT(fmt, args...)    /* Don't do anything in release builds */
+#endif
+
+#endif
