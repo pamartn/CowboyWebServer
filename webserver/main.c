721de901bbe9ea9aa05d33d26e3b87a6ee6fb899
Nicolas Pamart <nicolaspmt@gmail.com>
2016-02-11 07:25:32 +0100

    added 404 not found

--- webserver/main.c
+++ webserver/main.c
@@ -8,13 +8,13 @@
 #include <stdlib.h>
 #include "signals.h"
 #define BUF_SIZE 1024
-
+#define URL_SIZE 255
 
 /* 
 	Vérifie la première ligne du header http
 	Retourne 0 si echec, 1 si réussi
 */
-int verifier_ligne(char *ligne){
+int verifier_ligne(char *ligne, char *url){
 	int mots = 0;
 	char *token;
 	token = strtok(ligne, " ");
@@ -23,6 +23,8 @@ int verifier_ligne(char *ligne){
 		//On va maintenant analyser les mots
 		if(mots == 0 && !strcmp(token, "GET") == 0)
 			return 0;
+		if(mots == 1 && strlen(token) <= URL_SIZE)
+			strcpy(url, token);
 		if(mots == 2 && !(strncmp(token, "HTTP/1.0", 8) == 0 || strncmp(token, "HTTP/1.1", 8) == 0))
 			return 0;
 		// On récupère le prochain mot
@@ -32,17 +34,22 @@ int verifier_ligne(char *ligne){
 	return mots == 3;
 }
 
-void creer_reponse(char *res, int valid){
+void creer_reponse(char *res, char *url, int valid){
 	int content_length;		
 	char *msg;
-	if(valid){
+	if(valid && strcmp(url,"/") == 0){
 		msg = "<html><head><meta charset=\"UTF-8\"></head><h1>It works!</h1>Bonjour, bienvenue sur le serveur Cowboy. Ce serveur est créé pour remplacer Apache.\nLes cowboys ont tués les Indiens d'Amérique et se sont appropriés leurs terres.\n Le cow-boy ou cowboy, de l'anglais cow, « vache » et boy, « garçon »), qui signifie vacher ou bouvier en français, est un garçon de ferme s'occupant du bétail bovin dans les pays anglo-saxons de grands espaces comme le Far West américain et l'Outback australien.</html>";
 		content_length = strlen(msg);
 		sprintf(res, "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-length: %d\r\nContent-Type: text/html\r\n\r\n%s", content_length, msg);	
+	} else if(valid) {
+		msg = "404 Not found : ";
+		content_length = strlen(msg)+strlen(url);
+		sprintf(res, "HTTP/1.1 404 Not Found\r\nConnection: close\r\nContent-length: %d\r\n\r\n%s%s was not found on this server :(", content_length, msg, url);
 	} else {
 		msg = "400 Bad request";
 		content_length = strlen(msg);
 		sprintf(res, "HTTP/1.1 400 Bad Request\r\nConnection: close\r\nContent-length: %d\r\n\r\n%s", content_length, msg);
+	
 	}
 }
 
@@ -53,13 +60,13 @@ void dialoguer(int socket_client){
 	char buf[BUF_SIZE];
 
 	char res[2048];
-
+	char url[URL_SIZE];
 	if(fgets(buf, BUF_SIZE, f) != NULL){
 		printf("%s\n", buf);
-		if(verifier_ligne(buf))
-			creer_reponse(res, 1);
+		if(verifier_ligne(buf, url))
+			creer_reponse(res, url, 1);
 		else
-			creer_reponse(res, 0);
+			creer_reponse(res, url, 0);
 		while(strcmp(buf, "\n") != 0 && strcmp(buf, "\r\n") != 0)
 			fgets(buf, BUF_SIZE, f); 
 
